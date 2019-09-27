d34ef057658cd45c91c24dd0b54b13746bcca38c
Cole <cole+codebase@ournameismud.co.uk>
2019-09-27 14:38:44 +0100

    Fix schema

--- src/CurrencyLayer.php
+++ src/CurrencyLayer.php
@@ -57,7 +57,7 @@ class CurrencyLayer extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '1.0.1';
+    public $schemaVersion = '1.0.0';
 
     // Public Methods
     // =========================================================================
